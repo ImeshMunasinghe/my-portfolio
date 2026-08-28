439d774ff125de76281f908a7657613fb37a31fa
Imesh Munasinghe <imesh5lk@gmail.com>
2026-08-28 10:11:34 +0530

    chore: remove dead stub files Work.tsx and WritingPage.tsx (unregistered, empty pages)

--- src/pages/Work.tsx
+++
@@ -1,9 +0,0 @@
-import Projects from '../components/Projects/Projects';
-import { useDocumentTitle } from '../hooks/useDocumentTitle';
-
-function Work() {
-  useDocumentTitle('Work');
-  return <Projects />;
-}
-
-export default Work;
--- src/pages/WritingPage.tsx
+++
@@ -1,9 +0,0 @@
-import Writing from '../components/Writing/Writing';
-import { useDocumentTitle } from '../hooks/useDocumentTitle';
-
-function WritingPage() {
-  useDocumentTitle('Writing');
-  return <Writing />;
-}
-
-export default WritingPage;
